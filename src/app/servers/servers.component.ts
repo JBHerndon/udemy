5e2b6e5bee9b854c9b706688baf42548b9cfb750
James Herndon <jbhernd@ip-10-113-132-75.evoforge.org>
2022-08-26 17:36:51 +0000

    some changes

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '[app-servers]',
+  //selector: '[app-servers]',
+  selector:'app-servers',
   template: `
-    <app-server></app-server>
+    <app-server>dfdf</app-server>
     <app-server></app-server>`,
   styleUrls: ['./servers.component.scss']
 })
