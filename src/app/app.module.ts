67b7bd67a0aca9d848faa4814fb7c89fe81fa9d5
jbherndon <herndonja3@gmail.com>
2022-09-12 16:58:17 +0000

    Update 9/12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,6 +14,8 @@ import { ServersComponent } from './servers/servers.component';
     ServersComponent
   ],
   imports: [
+    CommonModule,
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
